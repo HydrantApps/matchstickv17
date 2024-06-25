9ea3ee9cfbadd7f6c77438cb26bbac7b2a095416
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2024-06-25 01:23:45 -0400

    release

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@
 //const {execSync} = require('child_process');
 import { execSync } from "child_process";
 const runCommand = (command) => {
+  //console.log('running command', command)
   try {
     execSync(`${command}`, { stdio: "inherit" });
   } catch (e) {
@@ -11,7 +12,7 @@ const runCommand = (command) => {
   }
   return true;
 };
-console.lof("test");
+console.log("test");
 const repoName = process.argv[2];
 const gitCheckoutCmd = `git clone --depth 1 https://github.com/HydrantApps/matchstickv17.git ${repoName}`;
 const installDeps = `cd ${repoName} && npm i`;
