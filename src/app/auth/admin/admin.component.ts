f789c55dc6619f0f1af85793268188e127834cf0
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2024-07-18 03:56:26 -0400

    updates to notification and admin

--- src/app/auth/admin/admin.component.ts
+++ src/app/auth/admin/admin.component.ts
@@ -1,12 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { from, Observable } from 'rxjs';
+import { UserProfile } from '../auth.types';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   providers: [],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
@@ -15,10 +25,34 @@ export class AdminComponent implements OnInit {
   authService = inject(AuthService);
   firestore = inject(Firestore);
   usersCollection = collection(this.firestore, 'users');
-  users$: Observable<any[]> | undefined;
+  users$: Observable<UserProfile[]> | undefined;
 
   ngOnInit(): void {
     //console.log(collectionData(this.usersCollection,{idField: 'id'}))
-    this.users$ = collectionData(this.usersCollection, { idField: 'id' });
+    this.users$ = collectionData(this.usersCollection, {
+      idField: 'id',
+    }) as Observable<UserProfile[]>;
   }
+
+  adminUpdateUser = (user: UserProfile) => {
+    const docRef = doc(this.firestore, 'users', user.id!);
+    updateDoc(docRef, { ...user })
+      .then((data) => {
+        //this.notifications.notify('Profile Updated Successfully')
+      })
+      .catch((error) => {
+        //this.notifications.notify('There was an error updaing the Profile')
+      });
+  };
+
+  deleteUser = (user: UserProfile) => {
+    const docRef = doc(this.firestore, `users/${user.id}`);
+    deleteDoc(docRef)
+      .then((data) => {
+        //this.notifications.notify('Profile Updated Successfully')
+      })
+      .catch((error) => {
+        //this.notifications.notify('There was an error updaing the Profile')
+      });
+  };
 }
--- src/app/shared/notification/notification.service.ts
+++ src/app/shared/notification/notification.service.ts
@@ -15,11 +15,31 @@ export enum NotificationTypeEnum {
   providedIn: 'root',
 })
 export class NotificationService {
+  userProfileNotifications = false;
   notification = signal<Notification>({});
-  constructor() {}
+  constructor() {
+    if (!('Notification' in window)) {
+      console.warn('This browser does not support desktop notification');
+    } else if (Notification.permission === 'granted') {
+      if (this.userProfileNotifications) {
+        const notification = new Notification('Welome to Matchstick');
+      }
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          const notification = new Notification(
+            'Thank you for Enabling Browser Notifications',
+          );
+        }
+      });
+    }
+  }
 
-  notify = (msg: string, NotificationType: NotificationTypeEnum) => {
+  notify = (msg: string, NotificationType: NotificationTypeEnum): void => {
     console.log('enum', NotificationType);
     this.notification.set({ message: msg, type: NotificationType });
+    if (this.userProfileNotifications) {
+      const notification = new Notification(msg);
+    }
   };
 }
